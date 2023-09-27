cfddaf109ea3ba67b846b8a130cbe70c952671b9
ex_zhangwenlei@exiot.cmcc <15196725410@139.com>
2023-09-27 16:33:27 +0800

    feat: gm-log examples

--- examples/react-project/vite.config.ts
+++ examples/react-project/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), UnoCSS(),],
+    base: '/gm-log/',
+    plugins: [react(), UnoCSS()],
 })
